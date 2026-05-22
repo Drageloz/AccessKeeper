59e645c5ee85ecc103c4b0f5e9decd17d99c8936
Drageloz <drageloz@siriland.com.co>
2026-05-22 09:25:04 -0500

    added gitignore

--- sites.js
+++ sites.js
@@ -3,33 +3,9 @@
 
 const SITES = [
   {
-    id: 'portal_dev',
-    name: 'Developer Portal',
-    url: 'https://dev.example.com',
-    description: 'Portal principal de desarrollo',
-  },
-  {
-    id: 'jira',
-    name: 'Jira',
-    url: 'https://company.atlassian.net',
-    description: 'Gestión de tickets',
-  },
-  {
-    id: 'confluence',
-    name: 'Confluence',
-    url: 'https://company.atlassian.net/wiki',
-    description: 'Documentación interna',
-  },
-  {
-    id: 'jenkins',
-    name: 'Jenkins',
-    url: 'https://jenkins.company.com',
-    description: 'CI/CD pipelines',
-  },
-  {
-    id: 'sonarqube',
-    name: 'SonarQube',
-    url: 'https://sonar.company.com',
-    description: 'Análisis de calidad de código',
+    id: 'crimson',
+    name: 'Crimson IST',
+    url: 'https://crimson-ist.com',
+    description: 'Crimson IST',
   },
 ];
